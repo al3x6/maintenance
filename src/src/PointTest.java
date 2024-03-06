1f6895247b30eee28dfdfd58dcecb419f3aabb7a
al3x6 <alexisar2002@gmail.com>
2024-03-06 18:11:32 +0100

    Test de classe

--- src/src/PointTest.java
+++ src/src/PointTest.java
@@ -1,15 +1,48 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PointTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void testToString() {
         Point p = new Point(2, 5);
-        assertEquals(p.toString(), "Point : 2, 5");
+        assertEquals( " Shape ID : 2 => Point : 2, 5 |", p.toString());
     }
 
-    @org.junit.jupiter.api.Test
-    void draw() {
+    @Test
+    void testDraw() {
+        Point p = new Point(2, 5);
+        ArrayList<Pixel> pixels = p.draw();
+        assertEquals(1, pixels.size());
+        assertEquals(new Pixel(2, 5), pixels.get(0));
+    }
 
+    @Test
+    void testGetP_x() {
+        Point p = new Point(2, 5);
+        assertEquals(2, p.getP_x());
+    }
+
+    @Test
+    void testGetP_y() {
+        Point p = new Point(2, 5);
+        assertEquals(5, p.getP_y());
+    }
+
+    @Test
+    void testSetP_x() {
+        Point p = new Point(2, 5);
+        p.setP_x(3);
+        assertEquals(3, p.getP_x());
+    }
+
+    @Test
+    void testSetP_y() {
+        Point p = new Point(2, 5);
+        p.setP_y(6);
+        assertEquals(6, p.getP_y());
     }
 }
--- src/src/RectangleTest.java
+++ src/src/RectangleTest.java
@@ -1,10 +1,13 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class RectangleTest {
 
     @Test
     void testToString() {
+        Point topLeft = new Point(2, 3);
+        Rectangle rectangle = new Rectangle(topLeft, 4, 5);
+        String expected = "Shape ID: 1 => Rectangle : Point : 2, 3, width 4, hauteur 5";
+        assertEquals(expected, rectangle.toString());
     }
 }
